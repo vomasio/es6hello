7a6f11f38fa5961d7f6bd34dea47485b69745c03
vomasio <zakicit@yahoo.co.jp>
2021-06-23 07:16:24 +0000

    webpack-dev-server を便利に設定する_修正001

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import Greeting from './hello.js';
+// import Greeting from './hello.js';
 
-const greeting = new Greeting();
-alert(greeting.sayHello());
+// const greeting = new Greeting();
+// alert(greeting.sayHello());
+
+export { default as Greeting } from './hello.js';
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,13 @@ module.exports = {
   mode: 'development',
   devServer: {
     disableHostCheck: true,
+    open: true,
+    openPage: "index.html",
+    contentBase: path.join(__dirname, "public"),
+    watchContentBase: true,
+    port: 8080,
+  },
+  output: {
+      libraryTarget: 'umd'
   }
 };
