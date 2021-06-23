3bad5c23b7db0d739cd7d617c399c28b48102050
vomasio <zakicit@yahoo.co.jp>
2021-06-23 06:58:27 +0000

    webpack-dev-server を便利に設定する

---
+++ src/hello.js
@@ -0,0 +1,12 @@
+export default class Hello {
+    constructor() {
+        
+    }
+    
+    /**greeting*/
+    sayHello() {
+        const hello = 'Hi,there(^^)';
+        console.log(hello);
+        return hello;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Greeting from './hello.js';
+
+const greeting = new Greeting();
+alert(greeting.sayHello());
